7e12681fcdfb90a00b02ca65dc24f7c02319d6cc
gitxiaobai <krisandpeters@qq.com>
2018-12-17 17:58:32 +0800

    "第一次提交这个信息，刚创建好项目"

---
+++ src/main/java/com/baizhi/springb1/Springb1Application.java
@@ -0,0 +1,14 @@
+package com.baizhi.springb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springb1Application.class, args);
+    }
+
+}
+
