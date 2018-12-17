bd31b0e2a74fbaf0361505a2d6e27a1dd47999d4
gitxiaobai <krisandpeters@qq.com>
2018-12-17 19:16:15 +0800

    "第二次提交项目,已经搭建好基础环境,maven-springboot-jsp-通用mapper-lombok-git"

---
+++ src/main/java/com/baizhi/springb1/dao/UserMapper.java
@@ -0,0 +1,7 @@
+package com.baizhi.springb1.dao;
+
+import com.baizhi.springb1.entity.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<%@page isELIgnored="false" contentType="text/html; utf-8" pageEncoding="utf-8" %>
+<html>
+<body>
+heheh
+</body>
+</html>
